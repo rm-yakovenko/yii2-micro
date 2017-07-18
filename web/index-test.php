61f9a87fff6592fbbda9bf4762dd80bd1ac8802e
Roman Yakovenko <erelus@gmail.com>
2017-07-18 17:29:06 +0300

    remove old copyright

--- web/index-test.php
+++ web/index-test.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * Copyright (c) 2017. HotCity
- *
- * Roman Yakovenko <erelus@gmail.com>
- */
 
 // NOTE: Make sure this file is not accessible when deployed to production
 if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
